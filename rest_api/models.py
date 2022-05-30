96e7e7fe654b311a69ee03ce90e9b3ea68cdb6d1
sedaliSedalian <seyedalian@outlook.com>
2022-05-30 10:17:03 +0430

    create model db

--- rest_api/models.py
+++ rest_api/models.py
@@ -1,3 +1,19 @@
 from django.db import models
 
 # Create your models here.
+class Symbol(models.Model):
+    TICKER = models.CharField(max_length=100)
+    DTYYYYMMDD = models.IntegerField(null=True , blank=True)
+    FIRST = models.IntegerField(null=True , blank=True)
+    HIGH = models.IntegerField(null=True , blank=True)
+    LOW = models.IntegerField(null=True , blank=True)
+    CLOSE = models.IntegerField(null=True , blank=True)
+    VALUE = models.IntegerField(null=True , blank=True)
+    VOL = models.IntegerField(null=True , blank=True)
+    OPENINT = models.IntegerField(null=True , blank=True)
+    PER = models.CharField(max_length=5)
+    OPEN = models.IntegerField(null=True , blank=True)
+    LAST = models.IntegerField(null=True , blank=True)
+
+    def __str__(self) -> str:
+        return self.TICKER
