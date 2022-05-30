de7248f66b13b35fd6012bdac067be50bee50aef
sedaliSedalian <seyedalian@outlook.com>
2022-05-30 10:33:21 +0430

    make serializer

---
+++ rest_api/serializers.py
@@ -0,0 +1,9 @@
+from attr import fields
+from rest_framework import serializers
+from .models import Symbol
+
+class SymbolSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Symbol
+        fields = '__all__'
+        
--- rest_api/views.py
+++ rest_api/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
+from rest_framework import viewsets
+from .models import Symbol
+from .serializers import SymbolSerializer
 
 # Create your views here.
+class SymbolView(viewsets.ModelViewSet):
+    queryset = Symbol.objects.all()
+    serializer_class = SymbolSerializer
+    
